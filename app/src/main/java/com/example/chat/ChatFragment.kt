81db6a7405437f128466c8ee3c9642a1ed78ed4a
KIrillPal <49895546+KIrillPal@users.noreply.github.com>
2024-04-14 17:40:20 +0300

    Part 1

---
+++ app/src/main/java/com/example/chat/ChatFragment.kt
@@ -0,0 +1,123 @@
+package com.example.chat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+private const val ARG_CHATNAME = "chatname"
+private const val ARG_CHATDESCR = "description"
+private const val ARG_CHATICONID = "iconId"
+
+fun timeToStamp(time: String): Long {
+    return SimpleDateFormat("HH:mm", Locale.US).parse(time)!!.time
+}
+
+class ChatFragment : Fragment() {
+    private var chatname: String? = null
+    private var chatdescr: String? = null
+    private var chaticonid: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            chatname = it.getString(ARG_CHATNAME)
+            chatdescr = it.getString(ARG_CHATDESCR)
+            chaticonid = it.getInt(ARG_CHATICONID)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.chat_fragment, container, false)
+        val chatNameView = view.findViewById<TextView>(R.id.chatname)
+        chatNameView.text = chatname
+
+        val chatDescrView = view.findViewById<TextView>(R.id.chatdescr)
+        chatDescrView.text = chatdescr
+
+        val chatIconView = view.findViewById<ShapeableImageView>(R.id.chaticon)
+        if (chaticonid != null) {
+            chatIconView.setImageResource(chaticonid!!)
+            chatIconView.setOnClickListener {
+                val profile_fragment =
+                    UserProfileFragment.newInstance(chatname!!, "online", chatdescr!!, chaticonid!!)
+                parentFragmentManager
+                    .beginTransaction()
+                    .add(R.id.main_fragment_container, profile_fragment)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+
+        val messagelist = view.findViewById<RecyclerView>(R.id.messagelist)
+
+        // Messages
+        val messageDataList = ArrayList<MessageData>()
+        messageDataList.add(MessageData("Hello there! Hi, Alice!", "Bob", R.drawable.kitty, timeToStamp("22:45"), R.layout.other_message))
+        messageDataList.add(MessageData("Ohi! How are you?", null, null, timeToStamp("22:47"), R.layout.my_message))
+        messageDataList.add(MessageData("More than perfect", "Bob", R.drawable.kitty, timeToStamp("22:47"), R.layout.other_message))
+        messageDataList.add(MessageData("wanna meme?", null, null, timeToStamp("22:48"), R.layout.other_message))
+        messageDataList.add(MessageData("i have some", null, null, timeToStamp("22:48"), R.layout.other_message))
+        messageDataList.add(MessageData("gimme", null, null, timeToStamp("22:48"), R.layout.my_message))
+        messageDataList.add(MessageData("yes, of course we want))", "Charlie", R.drawable.red_kitty, timeToStamp("22:49"), R.layout.other_message))
+        messageDataList.add(MessageData("A man pulls up to the curb and asks the policeman, \"Can I park here?\"\n" +
+                "\"No,\" says the cop.\n" +
+                "\"What about all these other cars?\"\n" +
+                "\"They didn't ask!\"", "Bob", R.drawable.kitty, timeToStamp("22:51"), R.layout.other_message))
+
+        messageDataList.add(MessageData("Hehe. If only it worked with taxes!","Charlie", R.drawable.red_kitty, timeToStamp("22:52"), R.layout.other_message))
+        messageDataList.add(MessageData("XD", null, null, timeToStamp("22:53"), R.layout.my_message))
+        messageDataList.add(MessageData("Do u have one more?", null, null, timeToStamp("22:53"), R.layout.my_message))
+
+        messageDataList.add(MessageData("Yeep. One sec", "Bob", R.drawable.kitty, timeToStamp("22:54"), R.layout.other_message))
+        messageDataList.add(MessageData("A farmer was sitting in the neighborhood bar getting drunk. A man came in and asked the farmer, «Hey, why are you sitting here on this beautiful day, getting drunk?» The farmer shook his head and replied, «Some things you just can’t explain.»\n" +
+                "«So what happened that’s so horrible?» the man asked as he sat down next to the farmer.\n" +
+                "«Well,» the farmer said, «today I was sitting by my cow, milking her. Just as I got the bucket full, she lifted her left leg and kicked over the bucket.»\n" +
+                "«Okay,» said the man, «but that’s not so bad.» «Some things you just can’t explain,» the farmer replied. «So what happened then?» the man asked. The farmer said, «I took her left leg and tied it to the post on the left.»\n" +
+                "«And then?»\n" +
+                "«Well, I sat back down and continued to milk her. Just as I got the bucket full, she took her right leg and kicked over the bucket.»\n" +
+                "The man laughed and said, «Again?» The farmer replied, «Some things you just can’t explain.» «So, what did you do then?» the man asked.\n" +
+                "«I took her right leg this time and tied it to the post on the right.»\n" +
+                "«And then?»\n" +
+                "«Well, I sat back down and began milking her again. Just as I got the bucket full, the stupid cow knocked over the bucket with her tail.»\n" +
+                "«Hmmm,» the man said and nodded his head. «Some things you just can’t explain,» the farmer said.\n" +
+                "«So, what did you do?» the man asked.\n" +
+                "«Well,» the farmer said, «I didn’t have anymore rope, so I took off my belt and tied her tail to the rafter. In that moment, my pants fell down and my wife walked in … Some things you just can’t explain.»",
+            null, null, timeToStamp("22:54"), R.layout.other_message))
+
+        messageDataList.add(MessageData("wow it was a looooooooong story", null, null, timeToStamp("22:56"), R.layout.my_message))
+        messageDataList.add(MessageData("but i laughed. It's really funny", null, null, timeToStamp("22:56"), R.layout.my_message))
+        messageDataList.add(MessageData("thx!", "Bob", R.drawable.kitty, timeToStamp("23:02"), R.layout.other_message))
+
+
+        with(messagelist) {
+            layoutManager = LinearLayoutManager(context)
+            adapter = ChatAdapter(messageDataList)
+        }
+        return view
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(name: String, description: String, iconId: Int) =
+            ChatFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_CHATNAME, name)
+                    putString(ARG_CHATDESCR, description)
+                    putInt(ARG_CHATICONID, iconId)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/chat/ChatListFragment.kt
@@ -0,0 +1,50 @@
+package com.example.chat
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
+
+class ChatListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.chatlist_fragment, container, false)
+
+        val chatlist = view.findViewById<RecyclerView>(R.id.chatlist)
+
+        // Chats
+        val chatDataList = ArrayList<ChatData>()
+        chatDataList.add(ChatData("Alice, Bob, Charlie", "thx!", timeToStamp("23:02"), 0, R.drawable.green_kitty, "3 участника"))
+        chatDataList.add(ChatData("Top memes", "This jokes are neccessary to see", timeToStamp("21:20"), 10, R.drawable.green_kitty, "25 участников"))
+        chatDataList.add(ChatData("Bob", "ok, bye", timeToStamp("12:56"), 0, R.drawable.kitty, "Хороший парень, вежливый, изучаю английский"))
+        chatDataList.add(ChatData("Новости", "Штраф за ковыряние в носу пришел парню из Махачкалы", timeToStamp("20:12"), 2, R.drawable.green_kitty, "1024 участника"))
+        chatDataList.add(ChatData("аче)", "Внимание, анекдот!", timeToStamp("23:40"), 5, R.drawable.green_kitty, "3334 участника"))
+
+        with(chatlist) {
+            layoutManager = LinearLayoutManager(context)
+            adapter = ChatListAdapter(chatDataList)
+        }
+
+        val settingsButton = view.findViewById<ShapeableImageView>(R.id.chatssettingsbutton)
+        settingsButton.setOnClickListener {
+            parentFragmentManager
+                .beginTransaction()
+                .add(R.id.main_fragment_container, SettingsFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        return view
+    }
+}
